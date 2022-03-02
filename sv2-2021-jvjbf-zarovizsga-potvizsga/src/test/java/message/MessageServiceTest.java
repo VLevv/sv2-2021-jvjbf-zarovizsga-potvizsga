3b6cfca880b6e93632a254278da003305df535a3
VLevv <vadaslevi@gmail.com>
2022-03-02 12:55:18 +0100

    end

--- sv2-2021-jvjbf-zarovizsga-potvizsga/src/test/java/message/MessageServiceTest.java
+++ sv2-2021-jvjbf-zarovizsga-potvizsga/src/test/java/message/MessageServiceTest.java
@@ -24,8 +24,8 @@ class MessageServiceTest {
         MariaDbDataSource dataSource = new MariaDbDataSource();
         try {
             dataSource.setUrl("jdbc:mariadb://localhost:3306/exam-test?useUnicode=true");
-            dataSource.setUserName("activitytracker");
-            dataSource.setPassword("activitytracker");
+            dataSource.setUserName("root");
+            dataSource.setPassword("training");
         } catch (SQLException sqle) {
             throw new IllegalStateException("Cannot reach DataBase!", sqle);
         }
